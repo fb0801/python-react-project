93a7842d78350cb7f2996c1ce44326ab88e97f63
fb0801 <fbhatti0801@gmail.com>
2024-02-27 18:12:11 +0000

    frontend 6

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import ContactForm from './ContactForm'
 
 function App() {
   const [contacts, setContacts] = useState([])
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [currentContact, setCurrentContact] = useState({})
 
 useEffect(() => {
   fetchContacts()
@@ -17,12 +19,28 @@ useEffect(() => {
 
   }  
 
+  const closeModal = () => {
+    setIsModalOpen(false)
+    setCurrentContact({})
+  }
+
+  const openCreateModal = () => {
+    if (!isModalOpen) setIsModalOpen(true)
+  }
+
   return (
     <>
-      <ContactList contacts={contacts}/>
-      <ContactForm />
+      <ContactList contacts={contacts} updateContact={openEditModal} updateCallback={onUpdate} />
+      <button onClick={openCreateModal}>Create New Contact</button>
+      {isModalOpen && <div className="modal">
+        <div className="modal-content">
+          <span className="close" onClick={closeModal}>&times;</span>
+          <ContactForm existingContact={currentContact} updateCallback={onUpdate} />
+        </div>
+      </div>
+      }
     </>
-  )
+  );
 }
 
-export default App
+export default App;
